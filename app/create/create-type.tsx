320e014386ba3fedbd4ecb4d8a632a6fd294b729
Kylian Dupouy Peron <kdperon@yahoo.com>
2024-10-15 08:06:19 +0200

    Refactor create form to add validation messages for choice inputs

--- app/create/create-type.tsx
+++ app/create/create-type.tsx
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const createType = z.object({
-  choice1: z.string().min(1).max(255),
-  choice2: z.string().min(1).max(255),
+  choice1: z.string({ required_error: "Le choix ne peut pas êtres vide", invalid_type_error: "Le choix ne peut pas êtres vide" }).min(1, "Le choix ne peut pas êtres vide").max(255, "Le choix doit faire moins de 255 caractères"),
+  choice2: z.string({ required_error: "Le choix ne peut pas êtres vide", invalid_type_error: "Le choix ne peut pas êtres vide" }).min(1, "Le choix ne peut pas êtres vide").max(255, "Le choix doit faire moins de 255 caractères"),
 })
