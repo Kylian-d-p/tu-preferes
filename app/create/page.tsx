b113b79f649cfbe48a0ca9186f0a5df877cf3a52
Kylian-d-p <kdperon@yahoo.com>
2024-10-15 16:33:25 +0200

    Refactor create form to improve layout and add playlist creation button

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,13 +1,18 @@
+import Link from "next/link";
 import CreateForm from "./create-form";
+import { Button } from "@/components/ui/button";
 
 export default async function CreateChoice() {
   return (
-    <main className="max-w-3xl mx-auto flex flex-col gap-5 mt-4">
+    <main className="max-w-3xl mx-auto flex flex-col items-center gap-5 mt-4">
       <div className="flex flex-col gap-2">
         <h1 className="text-3xl font-bold">Créer un Tu Préfères</h1>
         <p className="text-muted-foreground">Remplissez le formulaire ci-dessous afin de publier un &quot;Tu Préfères&quot;</p>
       </div>
       <CreateForm />
+      <Link href={"/create/playlist"} className="w-full">
+        <Button variant={"secondary"} className="w-full">Créer une playlist</Button>
+      </Link>
     </main>
   )
 }
