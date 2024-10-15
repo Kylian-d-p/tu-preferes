3b595773e0ae3fb55ef9f521f948fac45d28bd70
Kylian-d-p <kdperon@yahoo.com>
2024-10-15 18:16:23 +0200

    Add migration for creating playlist and playlist_choice tables

---
+++ prisma/migrations/20241015160812_add_playlists/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE `playlist` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` CHAR(255) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `playlist_choice` (
+    `id` VARCHAR(191) NOT NULL,
+    `playlistId` VARCHAR(191) NOT NULL,
+    `choiceId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
