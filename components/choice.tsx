2d84c40cfc5aeb7bd3b289fb7f4e8da0f68bff2a
Kylian Dupouy Peron <kdperon@yahoo.com>
2024-10-15 07:55:57 +0200

    Refactor choice component to improve performance and fix positioning bug

--- components/choice.tsx
+++ components/choice.tsx
@@ -14,7 +14,7 @@ export default function Choice(props: { onClick: () => void, label: string, vote
   }, [props.percentage]);
 
   return (
-    <div ref={ref} className={`flex flex-col items-center justify-center gap-2 transition-all duration-500 ${!props.showResults ? "hover:bg-secondary cursor-pointer" : (props.winner ? "bg-green-900" : "bg-red-900")} absolute z-0 overflow-hidden w-full lg:h-full ${props.showResults ? `h-[var(--percentage)] lg:w-[var(--percentage)] ${props.index && `top-[calc(100%-var(--percentage))] lg:left-[calc(100%-var(--percentage))] lg:top-0`}` : `h-1/2 lg:w-1/2 ${props.index ? "top-1/2 left-1/2 lg:top-0" : "top-0 left-0"}`}`} onClick={() => { props.onClick() }}>
+    <div ref={ref} className={`flex flex-col items-center justify-center gap-2 transition-all duration-500 ${!props.showResults ? "hover:bg-secondary cursor-pointer" : (props.winner ? "bg-green-900" : "bg-red-900")} absolute z-0 overflow-hidden w-full lg:h-full ${props.showResults ? `h-[var(--percentage)] lg:w-[var(--percentage)] ${props.index && `top-[calc(100%-var(--percentage))] lg:left-[calc(100%-var(--percentage))] lg:top-0`}` : `h-1/2 lg:w-1/2 ${props.index ? "top-1/2 left-0 lg:left-1/2 lg:top-0" : "top-0 left-0"}`}`} onClick={() => { props.onClick() }}>
       <div className="py-10 px-5 lg:px-10 flex flex-col items-center">
         <p className="text-3xl font-bold">{props.label}</p>
         <p className={`text-muted-foreground overflow-hidden duration-500 transition-all ${props.showResults ? "h-12" : "h-0"}`}>{props.votes} vote{props.votes >= 2 && "s"} ({props.percentage.toFixed(0)}%)</p>
