c296c86988dc6d481263d5e4116ac96d3e083d73
Kylian Dupouy Peron <kdperon@yahoo.com>
2024-10-15 12:34:48 +0200

    Refactor create form to update vote counts and percentages dynamically

--- app/choices.tsx
+++ app/choices.tsx
@@ -43,7 +43,7 @@ export default function Choices(props: { choice: { id: string, choice1: { label:
       <Choice onClick={() => { vote(1) }} label={props.choice.choice1.label} votes={votes.choice1} percentage={percentages.choice1} showResults={showResults} index={0} winner={percentages.choice1 > percentages.choice2} />
       <div className={`absolute font-bold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-background border border-foreground rounded-lg p-2 z-10 ${showResults && "opacity-0"}`}>OU</div>
       <Choice onClick={() => { vote(2) }} label={props.choice.choice2.label} votes={votes.choice2} percentage={percentages.choice2} showResults={showResults} index={1} winner={percentages.choice2 >= percentages.choice1} />
-      <div className={`absolute top-10 left-1/2 -translate-x-1/2 shadow flex items-center gap-2 duration-500 transition-all ${showResults ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0"}`}>
+      <div className={`absolute top-10 left-1/2 -translate-x-1/2 shadow flex items-center gap-2 duration-500 transition-all ${!showResults && "hidden"}`}>
         <Button
           className={"text-lg font-bold hover:scale-105 duration-500 transition-all flex items-center gap-2"}
           onClick={() => {
